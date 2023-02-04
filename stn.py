b84ae3fccba8e2964403c62926387c110fcdfcc5
charlieoneill11 <cponeill00@gmail.com>
2023-02-05 08:43:13 +1100

    added target as first fundus image

--- stn.py
+++ stn.py
@@ -1,90 +1,70 @@
 import os
-import re
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+import numpy as np
 
-class STN(nn.Module):
-    """
-    Spatial Transformer Network (STN)
-    """
-    def __init__(self, in_channels):
-        super(STN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=5, stride=1, padding=2)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 64 * 64, 1024)
-        self.fc2 = nn.Linear(1024, 6)
-
-    def forward(self, x):
-        x = torch.relu(self.conv1(x))
-        x = torch.relu(self.conv2(x))
-        x = x.view(-1, 64 * 64 * 64)
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
-        theta = x.view(-1, 2, 3)
-        return theta
-
-class FundusDataset(torch.utils.data.Dataset):
-    """
-    Dataset for retinal fundus images
-    """
+class FundusDataset(Dataset):
     def __init__(self, directory, patient_id, transform=None):
         self.directory = directory
         self.patient_id = patient_id
         self.transform = transform
-        self.files = self.get_files()
-        self.images = self.get_images()
-
-    def get_files(self):
-        """
-        Get all fundus image files for a given patient
-        """
-        pattern = f"^{self.patient_id}_.*_fundus_.*_(L|R)\.png$"
-        files = [f for f in os.listdir(self.directory) if re.match(pattern, f)]
-        return files
-
-    def get_images(self):
-        """
-        Get all fundus images for a given patient
-        """
-        images = [cv2.imread(os.path.join(self.directory, f), cv2.IMREAD_GRAYSCALE) for f in self.files]
-        return images
+        self.image_files = [f for f in os.listdir(directory) if f.startswith(patient_id)]
+        self.target_image = None
 
     def __len__(self):
-        return len(self.files)
+        return len(self.image_files)
 
     def __getitem__(self, idx):
-        image = self.images[idx]
+        image_file = self.image_files[idx]
+        image = plt.imread(os.path.join(self.directory, image_file))
         if self.transform:
             image = self.transform(image)
-        return image
+
+        if self.target_image is None:
+            self.target_image = image
+
+        return image, self.target_image
+
+class STN(nn.Module):
+    def __init__(self):
+        super(STN, self).__init__()
+        self.fc1 = nn.Linear(64 * 64, 32)
+        self.fc2 = nn.Linear(32, 3 * 2)
+
+    def forward(self, x):
+        x = x.view(-1, 64 * 64)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = x.view(-1, 2, 3)
+        return x
 
 def train(model, dataset, criterion, optimizer, num_epochs):
-    """
-    Train the model
-    """
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True, num_workers=4)
     for epoch in range(num_epochs):
-        for i, image in enumerate(dataloader):
-            theta = model(image)
-            loss = criterion(theta, image)
+        running_loss = 0.0
+        for i, (inputs, targets) in enumerate(dataset):
             optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
             loss.backward()
             optimizer.step()
-        print("Epoch: {}, Loss: {}".format(epoch, loss.item()))
+            running_loss += loss.item()
+        print('Epoch [%d/%d], Loss: %.4f' % (epoch + 1, num_epochs, running_loss / len(dataset)))
 
 def main():
-    """
-    Main function
-    """
-    directory = "path/to/fundus/images"
-    patient_id = "123456"
-    transform = transforms.Compose([transforms.ToTensor()])
+    directory = 'fundus_images'
+    patient_id = '001'
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize(64),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5])
+    ])
     dataset = FundusDataset(directory, patient_id, transform)
-    model = STN(1)
+    model = STN()
     criterion = nn.L1Loss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     num_epochs = 10
     train(model, dataset, criterion, optimizer, num_epochs)
 
