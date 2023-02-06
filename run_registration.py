033efde31daffbbf2b220fef04b85de163b3cf63
charlieoneill11 <cponeill00@gmail.com>
2023-02-06 11:21:54 +1100

    stn notebook work

--- run_registration.py
+++ run_registration.py
@@ -1,5 +1,7 @@
-import wandb
 import numpy as np
+from pathlib import Path
+import os
+import cv2
 from skimage.registration import optical_flow_tvl1, optical_flow_ilk
 from skimage.transform import warp
 from skimage.color import rgb2gray
@@ -7,7 +9,33 @@ from tqdm import tqdm
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import normalized_root_mse as nrmse
 
-from retrieve_images import retrieve_images
+import argparse
+
+parser = argparse.ArgumentParser(description='Script to handle command line arguments')
+parser.add_argument('--registration_algo', type=str, default="optical_flow", help='Algorithm to use for registration')
+args = parser.parse_args()
+
+def retrieve_images(patient_id = '156518'):
+    # Set the root directory for the patient data
+    root_dir = Path(f'data/{patient_id}')
+
+    # Get the list of image filenames for the left eye
+    image_filenames = [f for f in os.listdir(root_dir) if 'L.png' in f]
+
+    # Read the images into a list
+    images = [cv2.imread(str(root_dir / f)) for f in image_filenames]
+
+    # Convert the images to grayscale
+    gray_images = [rgb2gray(img) for img in images]
+
+    # Register all images to the first image
+    template = gray_images[0]
+
+    # Remove invalid images
+    final_images = [x for x in gray_images[1:] if x.shape == template.shape]
+
+    return final_images, template
+
 
 class RegistrationAlgorithm:
     
@@ -39,11 +67,6 @@ class RegistrationAlgorithm:
             ssim_value = ssim(self.template, registered_img, data_range=registered_img.max() - registered_img.min())
             ssim_values.append(ssim_value)
 
-        wandb.init(project='macuject-registration', entity="charlieoneill")
-        wandb.log({"L1 loss": np.mean(l1_losses), 
-                   "NCC": np.mean(ncc_values), 
-                   "SSIM": np.mean(ssim_values)}, 
-                   step=wandb.step, run_id=self.registration_function.__name__)
         return l1_losses, ncc_values, ssim_values
 
 
@@ -58,4 +81,14 @@ def optical_flow(template, img):
     registered = warp(img, np.array([row_coords + v, col_coords + u]), mode='edge')
     return registered
 
+### MAPPINGS ###
+algo_dict = {'optical_flow': optical_flow}
+ 
+
+if __name__ == "__main__":
+    opt = RegistrationAlgorithm(algo_dict[args.registration_algo])
+    l1_losses, ncc_values, ssim_values = opt.evaluate_registration()
+    print("L1 losses:", f"{np.mean(l1_losses):.2f}")
+    print("Normalized cross-correlation values:", f"{np.mean(ncc_values):.2f}")
+    print("Structural similarity index values:", f"{np.mean(ssim_values):.2f}")
     
